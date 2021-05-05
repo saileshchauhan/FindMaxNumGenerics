e5bf66195ed93fcabf252c3d7abfa6e55aa2b7a3
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-05 22:30:07 +0530

    [sailesh] Add:Max_Of_Floats

--- MaxOfThreeNumber.cs
+++ MaxOfThreeNumber.cs
@@ -25,5 +25,25 @@ namespace FindMaxNumGenerics
                 throw new Exception("FirstNumber,ThirdNumber,SecondNumber are Equal");
             }
         }
+        public static double Max_Of_Three_Floats(double firstNumber,double secondNumber,double thirdNumber)
+        {
+            if (firstNumber.CompareTo(thirdNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(thirdNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            else
+            {
+                throw new Exception("FirstNumber,ThirdNumber,SecondNumber are Equal");
+            }
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace FindMaxNumGenerics
             MaxOfThreeNumber integer = new MaxOfThreeNumber();
             int maximumInteger=integer.Max_Of_Three_Integers(23,45,78);
             Console.WriteLine("Maximum Integer is "+maximumInteger);
+
+            double maximumDecimal = MaxOfThreeNumber.Max_Of_Three_Floats(4.27,5.6,7.89);
+            Console.WriteLine("Maximum decimal number is "+maximumDecimal);
         }
     }
 }
