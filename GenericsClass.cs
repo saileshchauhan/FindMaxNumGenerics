e70a93f59af06ad8330bf7bc91d0487f60250a3d
saileshchauhan <chauhansailesh68@gmail.com>
2021-05-06 20:11:54 +0530

    [sailesh] Add: Generic Class

---
+++ GenericsClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaxNumGenerics
+{
+    class GenericsClass<T> where T : IComparable
+    {
+        private T first, second, third;
+        public GenericsClass(T first, T second, T third)
+        {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+        public T To_Find_Max_Using_Generics_Class()
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                throw new Exception("FirstNumber,ThirdNumber,SecondNumber are Equal");
+            }
+
+        }
+
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,15 +6,26 @@ namespace FindMaxNumGenerics
     {
         static void Main(string[] args)
         {
-            int largestInteger = MaxOfThreeNumber.To_Find_Max_Using_Generics<int>(6, 7, 4);
-            Console.WriteLine(largestInteger);
+            //int largestInteger = MaxOfThreeNumber.To_Find_Max_Using_Generics<int>(6, 7, 4);
+            //Console.WriteLine(largestInteger);
 
-            string longestString = MaxOfThreeNumber.To_Find_Max_Using_Generics<string>("zebra","lion","tiger");
-            Console.WriteLine(longestString);
+            //string longestString = MaxOfThreeNumber.To_Find_Max_Using_Generics<string>("zebra","lion","tiger");
+            //Console.WriteLine(longestString);
 
-            double largestFloat = MaxOfThreeNumber.To_Find_Max_Using_Generics<double>(2.4,5.789,4.56);
-            Console.WriteLine(largestFloat);
+            //double largestFloat = MaxOfThreeNumber.To_Find_Max_Using_Generics<double>(2.4,5.789,4.56);
+            //Console.WriteLine(largestFloat);
 
+            GenericsClass<int> maxInteger = new GenericsClass<int>(3, 5, 7);
+            int maximumOfThreeIntegers = maxInteger.To_Find_Max_Using_Generics_Class();
+            Console.WriteLine("Max of three Integers  "+maximumOfThreeIntegers);
+
+            GenericsClass<double> maxdecimal = new GenericsClass<double>(5.6, 7.09, 8.1);
+            double maximumOfThreeDecimals = maxdecimal.To_Find_Max_Using_Generics_Class();
+            Console.WriteLine("Max of three Decimal Number  "+maximumOfThreeDecimals);
+
+            GenericsClass<string> longString = new GenericsClass<string>("BridzeLabs", "Infosys", "wipro");
+            string longestString = longString.To_Find_Max_Using_Generics_Class();
+            Console.WriteLine("Longest String is "+longestString);
 
 
         }
